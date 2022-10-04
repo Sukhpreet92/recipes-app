5a6720a8633761ebe652b6d4fe90bb4409398535
Sukhpreet Singh <senti.kudi@yahoo.com>
2022-10-04 17:13:01 +0200

    add Veggie and Dessert page

---
+++ pages/dessert.js
@@ -0,0 +1,8 @@
+export default function Dessert() {
+  return (
+    <>
+      <h1>Hello my Dessert Try</h1>
+      <p> Hope it works yummy</p>
+    </>
+  );
+}
---
+++ pages/veggie.js
@@ -0,0 +1,8 @@
+export default function Veggie() {
+  return (
+    <>
+      <h1>Hello my Veggie Try</h1>
+      <p> Hope it works greeny</p>
+    </>
+  );
+}
